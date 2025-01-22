96c2636361f617c0cc3c4f64475a02f017572236
Oscar <jan.oscar.backman@gmail.com>
2025-01-22 20:24:05 +0100

    update to link in navbar

--- fitpro/src/components/Navbar.tsx
+++ fitpro/src/components/Navbar.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link';
+
 const Navbar = () => {
     return (
       <nav className="bg-blue-600 p-4">
         <ul className="flex justify-around">
-          <li><a href="/" className="text-white">Home</a></li>
-          <li><a href="/workouts" className="text-white">Workouts</a></li>
-          <li><a href="/progress" className="text-white">Progress</a></li>
-          <li><a href="/profile" className="text-white">Profile</a></li>
+          <li><Link href="/" className="text-white">Home</Link></li>
+          <li><Link href="/workouts" className="text-white">Workouts</Link></li>
+          <li><Link href="/progress" className="text-white">Progress</Link></li>
+          <li><Link href="/profile" className="text-white">Profile</Link></li>
         </ul>
       </nav>
     );
